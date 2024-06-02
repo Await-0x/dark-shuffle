326bb87431437f008d4eadf02ca807c3cd20b1e8
Await-0x <146035814+Await-0x@users.noreply.github.com>
2024-06-02 08:30:32 +0200

    config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,5 @@ slot deployments delete darkshuffle torii
 echo "Setting up remote indexer on slot..."
 slot deployments create darkshuffle torii --version 0.6.0 --world 0x1ea1b9ecad1655ffba513fc73818ce42fbbb36175151ead4a822c0aabb05eab --rpc https://api.cartridge.gg/x/realms/katana/ --start-block 0
 
-bun --env-file=../client/.env.production ../config/index.ts
-
 echo "Setting up config..."
 ./scripts/set_writer.sh --interval 1  --mode prod 
