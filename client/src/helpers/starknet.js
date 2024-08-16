d812f82acbd62b6136e15dccc4dbfd8ed2d2ecc1
Await-0x <146035814+Await-0x@users.noreply.github.com>
2024-08-16 09:40:40 -0400

    connector

--- client/src/helpers/starknet.js
+++ client/src/helpers/starknet.js
@@ -1,12 +1,59 @@
-import { jsonRpcProvider } from "@starknet-react/core";
-import { WebWalletConnector } from "@argent/starknet-react-webwallet-connector";
-import { argent, braavos } from "@starknet-react/core";
+import { argent, braavos, jsonRpcProvider } from "@starknet-react/core";
 
-export const argentWebWalletConnector = new WebWalletConnector({
-  url: "https://web.argent.xyz/",
-});
+import CartridgeConnector from "@cartridge/connector";
+import { dojoConfig } from "../../dojo.config";
 
-export const connectors = [argent(), braavos(), argentWebWalletConnector];
+const cartridgeConnector = (gameAddress, lordsAddress, ethAddress) =>
+  new CartridgeConnector(
+    [
+      {
+        target: gameAddress,
+        method: "new_game",
+      },
+      {
+        target: gameAddress,
+        method: "explore",
+      },
+      {
+        target: gameAddress,
+        method: "attack",
+      },
+      {
+        target: gameAddress,
+        method: "flee",
+      },
+      {
+        target: gameAddress,
+        method: "equip",
+      },
+      {
+        target: gameAddress,
+        method: "drop",
+      },
+      {
+        target: gameAddress,
+        method: "upgrade",
+      },
+      {
+        target: lordsAddress,
+        method: "approve",
+      },
+      {
+        target: lordsAddress,
+        method: "mint_lords",
+      },
+      {
+        target: ethAddress,
+        method: "approve",
+      },
+    ],
+  );
+
+export const connectors = () => [
+  cartridgeConnector(dojoConfig.gameAddress, dojoConfig.lordsAddress, dojoConfig.ethAddress),
+  argent(),
+  braavos()
+];
 
 export function getProvider() {
   if (import.meta.env.VITE_NODE_ENV === 'development') {
